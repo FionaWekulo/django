6e5053472aef41568c053373db7c20efac16805f
FionaWekulo <fionawekulo+1@gmail.com>
2024-06-24 06:55:39 +0300

    created a model called Tour to represent a table in the db for the app whch i a travel agency and defined its column constraints. Made migrations and migrated

--- learning/myapp/models.py
+++ learning/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Tour(models.Model):
+    #we need an origin country, destination, number of nights and the price for the tour
+    origin_country = models.CharField(max_length=64)
+    destination_country = models.CharField(max_length=64)
+    number_of_nights = models.IntegerField()
+    price = models.IntegerField()
+    
